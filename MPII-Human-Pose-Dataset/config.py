7d501e3ba47623df4c5d4232f61f634e21f2b8e3
comojin1994 <comojin1994@gmail.com>
2020-09-25 11:41:04 +0900

    Add Data parsing confing file

---
+++ MPII-Human-Pose-Dataset/config.py
@@ -0,0 +1,4 @@
+mpii_annot_path = f'../../../../Datasets/MPII/annotation/mpii_human_pose_v1_u12_1.mat'
+mpii_annot_json_path = f'../../../../Datasets/MPII/annotation/mpii_annotation.json'
+
+IMG_PATH = f'../../../../Datasets/MPII/images/'
